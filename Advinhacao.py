081d24a94a2416cfc98512300f6e3922fa6a5f59
WallaceQueiros <wallacequeiros13@hotmail.com>
2022-05-28 16:20:11 -0300

    Inserindo elif na aplicação jogo

--- Advinhacao.py
+++ Advinhacao.py
@@ -10,5 +10,10 @@ print(f'Você digitou {chute}')
 
 if number_secret == chute:
     print('PARABÉNS, VOCÊ ACERTOU!')
+
 else:
+    if chute > number_secret:
+        print('O número digitado é maior que o secreto')
+    elif chute < number_secret:
+        print('O número digitado é menor que o secreto')
     print('QUE PENA, VOCÊ ERROU!')
