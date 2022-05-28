28648cca5f77d006cef2431317a110fabad04e9f
WallaceQueiros <wallacequeiros13@hotmail.com>
2022-05-28 16:48:06 -0300

    Inserindo um contador e laço While

--- Advinhacao.py
+++ Advinhacao.py
@@ -1,19 +1,21 @@
 print('********************************')
 print('Bem vindo ao jogo de Adivinhação')
 print('********************************')
-
+cont = 0
 number_secret = 42
+while True:
 
-chute = int(input('Digite o seu número: '))
+    chute = int(input('Digite o seu número: '))
+    cont += 1
+    print(f'Você já tentou {cont} vezes')
+    print(f'Você digitou {chute}')
 
-print(f'Você digitou {chute}')
-
-if number_secret == chute:
-    print('PARABÉNS, VOCÊ ACERTOU!')
-
-else:
-    if chute > number_secret:
-        print('O número digitado é maior que o secreto')
-    elif chute < number_secret:
-        print('O número digitado é menor que o secreto')
-    print('QUE PENA, VOCÊ ERROU!')
+    if number_secret == chute:
+        print('PARABÉNS, VOCÊ ACERTOU!')
+        break
+    else:
+        if chute > number_secret:
+            print('O número digitado é maior que o secreto')
+        elif chute < number_secret:
+            print('O número digitado é menor que o secreto')
+        print('QUE PENA, VOCÊ ERROU!')
